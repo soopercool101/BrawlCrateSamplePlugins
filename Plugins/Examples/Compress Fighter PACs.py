8de64306eb3e0991c58ca0c63ad9e613cf901a85
soopercool101 <soopercoolstages@gmail.com>
2020-01-20 10:39:23 -0800

    Don't compress 00

--- Plugins/Examples/Compress Fighter PACs.py
+++ Plugins/Examples/Compress Fighter PACs.py
@@ -23,15 +23,16 @@ if folder:
             for file in subfolder.GetFiles():
                 # Get ##, Dark, Fake, Spy, and Alt costumes that aren't Etc/Final/Results/Entry files
                 if re.search(r"(?<![Ee][Tt][Cc])(?<![Ff][Ii][Nn][Aa][Ll])(?<![Rr][Ee][Ss][Uu][Ll][Tt])(?<![Ee][Nn][Tt][Rr][Yy])(\d\d|[Ss][Pp][Yy]|[Dd][Aa][Rr][Kk]|[Ff][Aa][Kk][Ee]|[Aa][Ll][Tt]\w).[Pp][Aa][Cc]$", file.Name) is not None:
-                    # Open the file
-                    BrawlAPI.OpenFile(file.FullName)
-                    if isinstance(BrawlAPI.RootNode, ARCNode):
-                        # Compress as ExtendedLZ77 if it isn't already
-                        if not BrawlAPI.RootNode.Compression == "ExtendedLZ77":
-                            BrawlAPI.RootNode.Compression = "ExtendedLZ77"
-                            BrawlAPI.SaveFile()
-                    # Close the file without displaying messages
-                    BrawlAPI.ForceCloseFile()
+                    if not file.Name.lower().EndsWith("00.pac"):
+                        # Open the file
+                        BrawlAPI.OpenFile(file.FullName)
+                        if isinstance(BrawlAPI.RootNode, ARCNode):
+                            # Compress as ExtendedLZ77 if it isn't already
+                            if not BrawlAPI.RootNode.Compression == "ExtendedLZ77":
+                                BrawlAPI.RootNode.Compression = "ExtendedLZ77"
+                                BrawlAPI.SaveFile()
+                        # Close the file without displaying messages
+                        BrawlAPI.ForceCloseFile()
     # Ask if you'd like to turn off the auto-compress fighter PAC option (Assumedly you would if using this)
     if setting:
         if not BrawlAPI.ShowYesNoPrompt("BrawlCrate is currently set to decompress PAC files by default. Would you like to change this behavior?", "Update Settings"):
