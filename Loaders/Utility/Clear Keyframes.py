14d4651f89086fa8eabbd8e2efe1b4fd74fb9235
soopercool101 <soopercoolstages@gmail.com>
2020-10-16 23:41:10 -0700

    Clear Keyframes script now supports clearing all for a CHR0

--- Loaders/Utility/Clear Keyframes.py
+++ Loaders/Utility/Clear Keyframes.py
@@ -1,24 +1,48 @@
-﻿from BrawlCrate.API import *
-from BrawlCrate.NodeWrappers import CHR0EntryWrapper
+from BrawlCrate.API import *
+from BrawlCrate.NodeWrappers import CHR0Wrapper, CHR0EntryWrapper
 from BrawlLib.SSBB.ResourceNodes import *
 from BrawlLib.Wii.Animations import *
 from System.Windows.Forms import ToolStripMenuItem
 
 def clear_translation(sender, event_args):
-    for x in range(0, BrawlAPI.SelectedNode.FrameCount):
-        BrawlAPI.SelectedNode.RemoveKeyframeOnlyTrans(x)
+    if isinstance(BrawlAPI.SelectedNode, CHR0Node):
+        for node in BrawlAPI.SelectedNode.Children:
+            clear_node_translation(node)
+    else:
+        clear_node_translation(BrawlAPI.SelectedNode)
     BrawlAPI.RefreshPreview()
+
+def clear_node_translation(node):
+    for x in range(0, node.FrameCount):
+        node.RemoveKeyframeOnlyTrans(x)
 
 def clear_scale(sender, event_args):
-    for x in range(0, BrawlAPI.SelectedNode.FrameCount):
-        BrawlAPI.SelectedNode.RemoveKeyframeOnlyScale(x)
+    if isinstance(BrawlAPI.SelectedNode, CHR0Node):
+        for node in BrawlAPI.SelectedNode.Children:
+            clear_node_scale(node)
+    else:
+        clear_node_scale(BrawlAPI.SelectedNode)
     BrawlAPI.RefreshPreview()
+
+def clear_node_scale(node):
+    for x in range(0, node.FrameCount):
+        node.RemoveKeyframeOnlyScale(x)
 
 def clear_rotation(sender, event_args):
-    for x in range(0, BrawlAPI.SelectedNode.FrameCount):
-        BrawlAPI.SelectedNode.RemoveKeyframeOnlyRot(x)
+    if isinstance(BrawlAPI.SelectedNode, CHR0Node):
+        for node in BrawlAPI.SelectedNode.Children:
+            clear_node_rotation(node)
+    else:
+        clear_node_rotation(BrawlAPI.SelectedNode)
     BrawlAPI.RefreshPreview()
 
-BrawlAPI.AddContextMenuItem(CHR0EntryWrapper, None, 'Clears Translation', None, ToolStripMenuItem('Clear Translation', None, clear_translation))
-BrawlAPI.AddContextMenuItem(CHR0EntryWrapper, None, 'Clears Rotation', None, ToolStripMenuItem('Clear Rotation', None, clear_rotation))
-BrawlAPI.AddContextMenuItem(CHR0EntryWrapper, None, 'Clears Scale', None, ToolStripMenuItem('Clear Scale', None, clear_scale))
+def clear_node_rotation(node):
+    for x in range(0, node.FrameCount):
+        node.RemoveKeyframeOnlyRot(x)
+
+BrawlAPI.AddContextMenuItem(CHR0Wrapper, None, 'Clears translation keyframes for all entries', None, ToolStripMenuItem('Clear Translation', None, clear_translation))
+BrawlAPI.AddContextMenuItem(CHR0Wrapper, None, 'Clears rotation keyframes for all entries', None, ToolStripMenuItem('Clear Rotation', None, clear_rotation))
+BrawlAPI.AddContextMenuItem(CHR0Wrapper, None, 'Clears scale keyframes for all entries', None, ToolStripMenuItem('Clear Scale', None, clear_scale))
+BrawlAPI.AddContextMenuItem(CHR0EntryWrapper, None, 'Clears translation keyframes', None, ToolStripMenuItem('Clear Translation', None, clear_translation))
+BrawlAPI.AddContextMenuItem(CHR0EntryWrapper, None, 'Clears rotation keyframes', None, ToolStripMenuItem('Clear Rotation', None, clear_rotation))
+BrawlAPI.AddContextMenuItem(CHR0EntryWrapper, None, 'Clears scale keyframes', None, ToolStripMenuItem('Clear Scale', None, clear_scale))
